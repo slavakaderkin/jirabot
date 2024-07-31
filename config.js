8a49da0019a34bf1c70f52995879e13f01339a39
Slava Kaderkin <slavakaderkin@gmail.com>
2024-07-31 18:38:19 +0400

    Add test url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   JIRA_BASE_URL: process.env.JIRA_BASE_URL,
   JIRA_USERNAME: process.env.JIRA_USERNAME,
   JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
-  WEBHOOK_URL: process.env.WEBHOOK_URL || 'https://muddy-glade-27725.pktriot.net/webhook/',
-  WEB_URL: process.env.WEB_URL || 'https://muddy-glade-27725.pktriot.net',
+  WEBHOOK_URL: process.env.WEBHOOK_URL || 'https://test.affnet.ru/webhook/',
+  WEB_URL: process.env.WEB_URL || 'https://test.affnet.ru',
   SERVER_PORT: 8443
 };
